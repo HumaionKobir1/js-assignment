0242ccef43bbdea8f9247d3f91fd6a226ba236bc
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-07 20:06:14 +0600

    js assignment all problems solved. Happy codinggit add .

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function mindGame(num){
     }
 }
 
-/* I do for this problem. First of all Multiply the input number by 3, add 10 to the result, subtract 2 from the result, subtract 5 from the result, and return the final result of this operation. */
+/*  I do for this problem. First of all Multiply the input number by 3, add 10 to the result, subtract 2 from the result, subtract 5 from the result, and return the final result of this operation. */
 
 
 
@@ -55,9 +55,6 @@ function isLGSeven (num) {
     }
 }
 
-const result = 15;
-console.log(isLGSeven(result));
-
 // The function takes a number as input and calculates the difference between the input and 7. If the difference is less than 7, it returns the difference, otherwise, it returns double the input.
 
 
@@ -85,6 +82,32 @@ function findingBadData (arrNum) {
 }
 
 // The function findingBadData takes an array of numbers as an argument and returns the count of negative numbers in the array. The function uses a loop to iterate over each element in the array, checks if the element is less than 0, and increments the badData count if it is.
+
+
+
+
+// Problem-5
+
+function gemsToDiamond (gem1, gem2, gem3){
+    const firstFriend = gem1 * 21;
+    const secondFriend = gem2 * 32;
+    const thirdFriend = gem3 * 43;
+    const total = firstFriend + secondFriend + thirdFriend;
+
+    if (typeof gem1 != 'number' || typeof gem2 != 'number' || typeof gem3 != 'number'){
+        return "please enter a valid number";
+    }
+
+    else if(total < 1000 * 2){
+        return total;
+    }
+
+    else if (total >= 1000 * 2){
+        return total - 2000;
+    }
+}
+
+// This function takes three inputs (gem1, gem2, and gem3), representing the number of gems each of three friends has. It then calculates the number of diamonds each friend can get based on the conversion rate of each friend. It sums up the total number of diamonds they can get and returns the result.
 
 
 
